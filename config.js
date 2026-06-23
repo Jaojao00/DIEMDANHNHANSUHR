31920d5e0a9bee051410fe10c73c80705f956445
jaojao00 <ntien055788@gmail.com>
2026-06-23 21:40:10 +0700

    Update API URL once more

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // =============================================
   // Dán URL Google Apps Script Web App vào đây sau khi deploy
   // Xem hướng dẫn trong README.md
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyZo_ixPELJvb4tJYJN1M0z5rYh_xW1YM-mekVzu-a2WyYBoQHtUmbYHG4mtGghSz5d/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby1pMkIzhHZGHbWXukTeju_bMSzpxe2YarUB2OhwaR_0B5Tem1-dVA6lDvF7T8Fk96P/exec',
 
   // Tự động BẬT realtime nếu APPS_SCRIPT_URL đã được cài đặt
   get DEMO_MODE() {
