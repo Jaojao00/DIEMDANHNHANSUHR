ac91f8d9ca8eb66a72ece4b5a6bfa5601f66a8b4
Tài Dev Nguyễn <143507847+Jaojao00@users.noreply.github.com>
2026-07-17 18:43:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // =============================================
   // Dán URL Google Apps Script Web App vào đây sau khi deploy
   // Xem hướng dẫn trong README.md
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby0cV711qbVrUb8tYzRTbvTN7RJDliIgjm8hkyVsqX0u73SCkFvsIJxFsw9nhv-bCkT/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyJQpBV9dvHBVZ4U3YgVD6vhXVgbe3yJmENlRM7cEbXGsoXYfbfRkm_B78alo1eTJx_/exec',
 
   // Tự động BẬT realtime nếu APPS_SCRIPT_URL đã được cài đặt
   get DEMO_MODE() {
